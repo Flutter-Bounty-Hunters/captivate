c608f18d3e9cfd34a3b5755d4d5730531f221130
Matt Carroll <matthew-carroll@users.noreply.github.com>
2025-02-07 16:54:26 -0800

    Created a Media entity, and tried to create a progress tracking result for uploads.

--- lib/src/captivate_client.dart
+++ lib/src/captivate_client.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:captivate/src/episode.dart';
 import 'package:captivate/src/logging.dart';
+import 'package:captivate/src/media.dart';
 import 'package:captivate/src/show.dart';
 import 'package:captivate/src/user.dart';
 import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+import 'package:path/path.dart';
 
 class CaptivateClient {
   Future<UserPayload?> authenticate({
@@ -182,27 +186,43 @@ class CaptivateClient {
     }
   }
 
-  Future<void> uploadMedia({
+  Future<UploadProgressWithResult<Media>> uploadMedia({
     required String authToken,
     required String showId,
     required File mediaFile,
   }) async {
     CaptivateLogs.network.info("Upload media - show: $showId, media file: $mediaFile");
+
+    // Create a progress tracker, which wraps around a byte stream to the media file,
+    // so that we can monitor the progress of the upload.
+    //
+    // We'll pass this progress tracker to the HTTP client, instead of the File.
+    final progressTrackingUploadStream = HttpFileUploadStream(mediaFile);
+
+    // Assemble a multi-part HTTP request to upload the media file.
     var request = MultipartRequest(
       'POST',
       Uri.parse('https://api.captivate.fm/shows/$showId/media'),
     );
     request.files.add(
-      await MultipartFile.fromPath('file', mediaFile.path),
+      MultipartFile(
+        'file',
+        progressTrackingUploadStream.uploadByteStream,
+        progressTrackingUploadStream.totalByteCount,
+        filename: basename(mediaFile.path),
+      ),
     );
     request.headers.addAll({
       "Authorization": "Bearer $authToken",
       "Content-Type": "multipart/form-data",
     });
 
-    StreamedResponse response = await request.send();
+    // Execute the request.
+    Future<StreamedResponse> futureResponse = request.send();
 
-    if (response.statusCode == 200) {
+    // Wrap up the progress stream and the HTTP response into a result for the caller.
+    final result = UploadProgressWithResult(progressTrackingUploadStream, futureResponse, (response) async {
+      // Parse the response data.
       final body = await response.stream.bytesToString();
       final bodyJson = jsonDecode(body);
 
@@ -211,22 +231,146 @@ class CaptivateClient {
         throw Exception("Failed to upload media (${mediaFile.path}) - API error.");
       }
 
-      // Response structure:
-      // {
-      //     "success": true,
-      //     "media": {
-      //         "id": "mmmmmmmm-mmmm-mmmm-mmmm-mmmmmmmmmmmm",
-      //         .....
-      //     }
-      // }
-
       CaptivateLogs.network.fine("Success:\n${const JsonEncoder.withIndent(" ").convert(bodyJson)}");
-    } else {
-      _logNetworkError(response);
-      throw Exception("Failed to upload media (${mediaFile.path}) - network error.");
+      return Media.fromJson(bodyJson);
+    });
+
+    result.onComplete.then((_) {
+      if (result.wasSuccessful != true) {
+        CaptivateLogs.network.fine("Failed to upload media (${mediaFile.path}) - network error: ${result.error}");
+      }
+    });
+
+    return result;
+  }
+}
+
+class UploadProgressWithResult<EntityType> with Listenable {
+  UploadProgressWithResult(this._uploadStream, this._response, this._parser) {
+    _uploadStream.addListener(notifyListeners);
+    _response.then(_onUploadComplete).catchError(_onError);
+  }
+
+  @override
+  void dispose() {
+    _uploadStream.dispose();
+    super.dispose();
+  }
+
+  final HttpFileUploadStream _uploadStream;
+  final Future<StreamedResponse> _response;
+  final EntityParser<EntityType> _parser;
+
+  void _onUploadComplete(StreamedResponse response) async {
+    _isDone = true;
+    if (200 < response.statusCode || response.statusCode >= 300) {
+      _wasSuccessful = false;
+      _error = "HTTP Error (${response.statusCode}): ${response.reasonPhrase}";
+      _onComplete.complete();
+      notifyListeners();
+      return;
+    }
+
+    try {
+      _responseEntity = await _parser(response);
+    } catch (exception) {
+      _wasSuccessful = false;
+      _error = exception;
+    }
+
+    _onComplete.complete();
+    notifyListeners();
+  }
+
+  void _onError(Object error) {
+    _isDone = true;
+    _wasSuccessful = false;
+    _error = error;
+
+    _onComplete.complete();
+    notifyListeners();
+  }
+
+  double get progress => _uploadStream.progress;
+
+  Future<void> get onComplete => _onComplete.future;
+  final _onComplete = Completer();
+
+  bool get isDone => _isDone;
+  var _isDone = false;
+
+  bool? get wasSuccessful => _wasSuccessful;
+  bool? _wasSuccessful;
+
+  EntityType? get responseEntity => _responseEntity;
+  EntityType? _responseEntity;
+
+  Object? get error => _error;
+  Object? _error;
+}
+
+typedef EntityParser<EntityType> = FutureOr<EntityType?> Function(StreamedResponse);
+
+class HttpFileUploadStream<ResponseType> with Listenable {
+  HttpFileUploadStream(this._file) {
+    totalByteCount = _file.lengthSync();
+
+    // Wrap the file access in our own byte stream so we can track the upload progress.
+    final fileStream = _file.openRead();
+    uploadByteStream = ByteStream(
+      fileStream.transform(
+        StreamTransformer<List<int>, List<int>>.fromHandlers(
+          handleData: (data, sink) {
+            // Update our upload byte tracking.
+            _uploadedByteCount += data.length;
+
+            // Send the data to the server.
+            sink.add(data);
+
+            // Notify our listeners about the progress update.
+            notifyListeners();
+          },
+        ),
+      ),
+    );
+  }
+
+  final File _file;
+
+  late final ByteStream uploadByteStream;
+
+  double get progress => uploadedByteCount / totalByteCount;
+
+  int get uploadedByteCount => _uploadedByteCount;
+  int _uploadedByteCount = 0;
+
+  late final int totalByteCount;
+}
+
+mixin class Listenable {
+  void dispose() {
+    _listeners.clear();
+  }
+
+  final _listeners = <VoidCallback>{};
+
+  void addListener(VoidCallback listener) {
+    _listeners.add(listener);
+  }
+
+  void removeListener(VoidCallback listener) {
+    _listeners.remove(listener);
+  }
+
+  @protected
+  void notifyListeners() {
+    for (final listener in _listeners) {
+      listener();
     }
   }
 }
+
+typedef VoidCallback = void Function();
 
 void _logApiError(String goal, Map<String, dynamic> responseBody) {
   CaptivateLogs.network.warning("Failed to '$goal'.");
---
+++ lib/src/media.dart
@@ -0,0 +1,80 @@
+class Media {
+  static Media fromJson(Map<String, dynamic> json) {
+    return Media(
+      id: json["id"],
+      createdAt: json["created_at"],
+      updatedAt: json["updated_at"],
+      mediaName: json["media_name"],
+      showsId: json["shows_id"],
+      usersId: json["users_id"],
+      type: json["type"],
+      mediaType: json["media_type"],
+      mediaSize: json["media_size"],
+      mediaUrl: json["media_url"],
+      mediaBitRate: json["media_bit_rate"],
+      mediaDuration: json["media_duration"],
+      mediaId3Size: json["media_id3_size"],
+      objectStorageLocation: json["object_storage_location"],
+      // TODO: "integrated_lufs" when we figure out what data type it is (we're getting `null` in json).
+      nonCdnUrl: json["non_cdn_url"],
+    );
+  }
+
+  // {
+//  "success": true,
+//  "media": {
+//   "id": "143aa6a4-d1e6-4420-bdd4-f448363689e3",
+//   "media_name": "flutter-spaces-2025-01-22-hungrimind-ft-tadas-and-robert-cut-converted",
+//   "media_size": 110297616,
+//   "media_url": "https://podcasts.captivate.fm/media/143aa6a4-d1e6-4420-bdd4-f448363689e3/flutter-spaces-2025-01-22-hungrimind-ft-tadas-and-robert-cut-co.mp3",
+//   "shows_id": "3743ba71-859c-4164-99ff-999b525ccf48",
+//   "users_id": "f26aae97-5a30-403e-9176-d6194016976d",
+//   "created_at": "2025-02-06 08:03:08",
+//   "updated_at": "2025-02-06 08:03:08",
+//   "media_bit_rate": 128000,
+//   "media_duration": 6893.592,
+//   "media_id3_size": 144,
+//   "media_type": "audio/mpeg",
+//   "type": "audio",
+//   "object_storage_location": "media",
+//   "integrated_lufs": null,
+//   "non_cdn_url": "https://media-hosting.us-mia-1.linodeobjects.com/media/143aa6a4-d1e6-4420-bdd4-f448363689e3/flutter-spaces-2025-01-22-hungrimind-ft-tadas-and-robert-cut-co.mp3"
+//  }
+// }
+
+  const Media({
+    this.id,
+    this.createdAt,
+    this.updatedAt,
+    this.mediaName,
+    this.showsId,
+    this.usersId,
+    this.type,
+    this.mediaType,
+    this.mediaSize,
+    this.mediaUrl,
+    this.mediaBitRate,
+    this.mediaDuration,
+    this.mediaId3Size,
+    this.objectStorageLocation,
+    this.nonCdnUrl,
+  });
+
+  final String? id;
+  final String? createdAt; // e.g., "2025-02-06 08:03:08"
+  final String? updatedAt; // e.g., "2025-02-06 08:03:08"
+  final String? mediaName; // e.g., "flutter-spaces-2025-01-22-hungrimind-ft-tadas-and-robert-cut-converted"
+
+  final String? showsId;
+  final String? usersId;
+
+  final String? type; // e.g., "audio
+  final String? mediaType; // e.g., "audio/mpeg"
+  final int? mediaSize;
+  final String? mediaUrl;
+  final int? mediaBitRate;
+  final double? mediaDuration;
+  final int? mediaId3Size;
+  final String? objectStorageLocation;
+  final String? nonCdnUrl;
+}
